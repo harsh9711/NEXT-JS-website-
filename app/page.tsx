dafca931cc0c6d21ed793349d7b413cb245984af
harsh9711 <87559341+harsh9711@users.noreply.github.com>
2024-03-08 14:57:20 +0530

    done project

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
+import Features from "./components/Features";
+import Faq from "./components/Faq";
+import Cta from "./components/Cta";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
 export default function Home() {
   return (
     <div>
      <Navbar/>
      <Hero/>
+      <div className="px-[20px] lg:container lg:px-20 mx-auto">
+      <Features/>
+      <Faq/>
+      <Cta/>
+       <Pricing/>
+       <Footer/>
+     </div>
     </div>
   );
 }
